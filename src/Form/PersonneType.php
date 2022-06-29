3f3c07cc33b06f1a75c18bc0b38b02fd3c3cfb00
alltheatreides <goldschmidt.antoine@gmail.com>
2022-06-29 13:41:50 +0200

    Content: Form French Phone number validation

--- src/Form/PersonneType.php
+++ src/Form/PersonneType.php
@@ -4,20 +4,40 @@ namespace App\Form;
 
 use App\Entity\Personne;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class PersonneType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('datedenaissance')
-            ->add('telephone')
-            ->add('email')
-        ;
+            ->add('nom', TextType::class, [
+                'required' => true,
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+            ])
+            ->add('datedenaissance', DateTimeType::class, [
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'dd-MM-yyyy',
+            ])
+            ->add('telephone', TelType::class, [
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/',
+                        'message' => 'Vous avez rentré un mauvais numéro ou un numéro incorrect à l\'indicatif français.',
+                    ]),
+                ],
+                'help' => 'Le numéro de téléphone doit commencer par l\'indicatif français (+33).',
+            ])
+            ->add('email', EmailType::class);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
